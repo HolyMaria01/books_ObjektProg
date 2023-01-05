0880aeff2457429ab5cb93cbc4ea834805ceccbd
Maarja <maarja-liis.kaldvee@voco.ee>
2023-01-05 14:01:58 +0200

    Delete single book from localStorage

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ form.addEventListener('submit', addBook)
 
 document.addEventListener('DOMContentLoaded', getBooks)
 
+const booksTabel = document.querySelector('#books')
+booksTabel.addEventListener('click', deleteBook)
+
+function deleteBook (event){
+    if(event.target.textContent === 'X'){
+        if (confirm("Do you want to remove this book?")) {
+            event.target.parentElement.parentElement.remove()
+            const isbn = event.target.parentElement.previousElementSibling
+            const author = event.target.parentElement.previousElementSibling.previousElementSibling
+            const title = event.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling
+
+            const book = [title.textContent, author.textContent, isbn.textContent]
+
+            let books // array for LS data
+            if(localStorage.getItem('books') === null){
+                books = []
+            } else {
+                books = JSON.parse(localStorage.getItem('books'))
+            }
+            books.forEach(function(bookFromLS, index) {
+                if (bookFromLS[0] === book[0] && bookFromLS[1] === book[1] && bookFromLS[2] === book[2]){
+                    books.splice(index, 1)
+             }
+            })
+        localStorage.setItem('books', JSON.stringify(books))
+        }
+    }
+}
 function getBooks(){
     let books // array for LS data
     if(localStorage.getItem('books') === null){
@@ -54,5 +82,7 @@ function addBook(event){
     document.querySelector('#author').value = ''
     document.querySelector('#isbn').value = ''
     event.preventDefault()
+
+
 }
 
